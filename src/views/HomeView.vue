0e546f335cdcade1073821bfb3e4818f92f96df4
techatha <techatham_p@cmu.ac.th>
2024-07-12 00:38:15 +0700

    9.3 show to staff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -42,6 +42,8 @@ const events = ref<Event[]>([
 </script>
 
 <template>
+  <h1>Events for Good</h1>
+  <!-- new element -->
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
