39f3d4d9812ae76df663f84a110056ae9dc4ce2c
techatha <techatham_p@cmu.ac.th>
2024-07-30 16:31:55 +0700

    4.2 show register to the staff

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import { type Event } from '@/types'
+import { useRouter } from 'vue-router';
 
 const props = defineProps<{
   event: Event
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
+const router = useRouter()
+const register = () => {
+  // If the registration API call sucessful
+  // Push back to the event details view
+  router.push({ name: 'event-detail-view'})
+}
 </script>
 <template>
   <p>register event here</p>
+  <button @click="register">Register</button>
 </template>
