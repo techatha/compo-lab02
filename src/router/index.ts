12ce498014ee264eeeb2f10ad99b3665a8db2c28
techatha <techatham_p@cmu.ac.th>
2024-07-30 17:23:37 +0700

    5.2 close internet then click on any link to see NetworkError view

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,7 @@ import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 import StudentView from '@/views/StudetView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -58,6 +59,11 @@ const router = createRouter({
       name: '404-resource-view',
       component: NotFoundView,
       props: true
+    },
+    {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView
     },
     {
       path: '/:catchAll(.*)',
