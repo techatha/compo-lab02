4b74740a4f46c554bafb0eaee5a5d0d3e6a37cb3
techatha <techatham_p@cmu.ac.th>
2024-08-12 13:44:27 +0700

    3.3 explain each tailwind css

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const { message } = storeToRefs(store)
 </script>
 
 <template>
-  <div id="layout">
+  <div id="text-center font-sans text-gray-700 antialias">
     <header>
       <div id="flashMessage" v-if="message">
         <h4>{{ message }}</h4>
@@ -27,13 +27,6 @@ const { message } = storeToRefs(store)
 </template>
 
 <style>
-#layout {
-  font-family: Avenir, Arial, Helvetica, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smooothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 nav {
   padding: 30px;
