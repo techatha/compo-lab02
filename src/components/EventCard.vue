d0a6cf3741675acd3df58539629f39bb9c66748f
techatha <techatham_p@cmu.ac.th>
2024-08-12 13:55:50 +0700

    5. explain meaning of each class

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -17,7 +17,7 @@ defineProps<{ event: Event }>()
 <template>
   <RouterLink class="event-link" :to="{ name: 'event-detail-view', params: { id: event.id } }">
     <div class="event-class">
-      <div class="event-card">
+      <div class="cursor-pointer border border-gray-600 p-4 w-64 mb-6 hover:scale-101 hover:shadow-sp">
         <h2>{{ event.title }}</h2>
         <span>@{{ event.time }} on {{ event.date }}</span>
       </div>
@@ -32,10 +32,6 @@ defineProps<{ event: Event }>()
   cursor: pointer;
   border: 1px solid #39495c;
   margin-bottom: 18px;
-}
-.event-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
 .event-link {
   text-decoration: none;
